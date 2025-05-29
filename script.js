3ebe98500dcd3648e44ae3a4c32214467d106752
SayeemZaman3 <sayeem312@gmail.com>
2025-05-29 21:35:55 +0600

    add error handler

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const apiKey = 'ce6556fcf4e452a207fe0735729d4b03';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
 async function getWeather(city) {
-    const rawData = await fetch(apiUrl + `${city}&appid=${apiKey}`);
-    let data = await rawData.json();
-    
-    setWeather(data);
+    try {
+        const rawData = await fetch(apiUrl + `${city}&appid=${apiKey}`);
+        let data = await rawData.json();
+        
+        setWeather(data)
+    } catch (err) {
+        $('#error').css('display', 'block');
+        $('#error').text('Please enter a proper city name!');
+    }
 }
 
 function setWeather(data) {
