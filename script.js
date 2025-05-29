222423d86fc15586e900d744e6ca43d9c45e612b
SayeemZaman3 <sayeem312@gmail.com>
2025-05-29 21:20:14 +0600

    add get & set weather

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const cityInput = document.querySelector('input').value;
-const search = document.querySelector('form');
+const search = $('form');
 
 const apiKey = 'ce6556fcf4e452a207fe0735729d4b03';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-async function getWeatherData(city) {
-    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+async function getWeather(city) {
+    const rawData = await fetch(apiUrl + `${city}&appid=${apiKey}`);
     let data = await rawData.json();
-        
     
+    setWeather(data);
 }
+
+function setWeather(data) {
+    $('#tmp').text(data.main.temp + "C");
+    $('#name').text(data.name);
+}
+
+
+search.submit(e => {
+    e.preventDefault();
+    
+    const cityInput = $('#city').val().trim();
+    getWeather(cityInput);
+})
